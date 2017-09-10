ed7f0568792a3956077ad41c629fd471f295b1dc
Turan <turandai@foxmail.com>
2017-09-11 00:55:43 +0800

    Update MyPanel.py

--- MyPanel.py
+++ MyPanel.py
@@ -3,7 +3,6 @@ import wx
 class FullWindowPanel(wx.Panel):
     def __init__(self,parent_frame,panel_stack):
         wx.Panel.__init__(self,parent_frame,size=(300,300))
-        self.Bind(wx.EVT_KEY_UP, self.reicite_react_key)
         self.panel_stack=panel_stack
         wx.Panel.Show(self,False)
 
@@ -21,14 +20,3 @@ class FullWindowPanel(wx.Panel):
             print(self.panel_stack)
         if len(self.panel_stack):
             wx.Panel.Show(self.panel_stack[-1],True)
-
-
-
-
-
-
-    def reicite_react_key(self,event):
-        #print(event.GetKeyCode())
-        print(0)
-        if event.GetKeyCode()==27:
-            self.Show(False)
