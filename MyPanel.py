4584126279038e5627c573c718c4ab20da8a0957
Turan <turandai@foxmail.com>
2017-09-11 13:13:41 +0800

    Update MyPanel.py

--- MyPanel.py
+++ MyPanel.py
@@ -1,8 +1,8 @@
 import wx
 
-class FullWindowPanel(wx.Panel):
-    def __init__(self,parent_frame,panel_stack):
-        wx.Panel.__init__(self,parent_frame,size=(300,300))
+class BasePanel(wx.Panel):
+    def __init__(self,parent_frame,panel_stack,size=(300,300)):
+        wx.Panel.__init__(self,parent_frame,size=size)
         self.panel_stack=panel_stack
         wx.Panel.Show(self,False)
 
@@ -13,10 +13,8 @@ class FullWindowPanel(wx.Panel):
             if self in self.panel_stack:
                 self.panel_stack.remove(self)
             self.panel_stack.append(self)
-            print(self.panel_stack)
         else:
-            if self == self.panel_stack[-1]:
+            if self==self.panel_stack[-1]:
                 self.panel_stack.remove(self)
-            print(self.panel_stack)
         if len(self.panel_stack):
             wx.Panel.Show(self.panel_stack[-1],True)
